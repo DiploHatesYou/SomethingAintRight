f7b4ae0924a32f77273d59b7c6f8ce33176b114c
DiploHatesYou <56423538+DiploHatesYou@users.noreply.github.com>
2019-11-16 14:47:06 -0800

    Project upload

---
+++ Assets/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform player;
+
+    public float moveSpeed = 3;
+    public float maxDist = 15;
+    public float minDist;
+
+
+    void Update()
+    {
+        Follow();
+    }
+
+    void Follow()
+    {
+        transform.LookAt(player);
+
+        if (Vector3.Distance(transform.position, player.position) >= minDist)
+        {
+
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        }
+    }
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.tag == "Player")
+    //    {
+    //        Debug.Log("Enemy Hit");
+    //        collision.transform.Translate(0, 0, -3, Space.Self);
+    //    }
+    //}
+}
---
+++ Assets/Punching.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Punching : MonoBehaviour
+{
+    private bool punching = false;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (!punching && Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(Punch(0.5f, 1.25f, transform.forward));
+        }
+    }
+
+    IEnumerator Punch(float time, float distance, Vector3 direction)
+    {
+        punching = true;
+        var timer = 0.0f;
+        var orgPos = transform.position;
+        direction.Normalize();
+        Debug.Log("Above the loop");
+        while (timer <= time)
+        {
+            Debug.Log("----");
+            rb.MovePosition(orgPos + (Mathf.Sin(timer / time * Mathf.PI) + 1.0f) * direction);
+            //transform.position = orgPos + (Mathf.Sin(timer / time * Mathf.PI) + 1.0f) * direction;
+            yield return null;
+            timer += Time.deltaTime;
+        }
+        transform.position = orgPos;
+        punching = false;
+    }
+}
