24e5ff61b1eaa1696e65d658de87026fb731ed3d
DiploHatesYou <56423538+DiploHatesYou@users.noreply.github.com>
2019-11-18 17:22:08 -0800

    Cleaned up scripts and added player stats scipt

--- Assets/Scripts/Punch.cs
+++ Assets/Scripts/Punch.cs
@@ -1,23 +1,17 @@
-﻿using UnityEngine;
-using UnityEngine.AI;
+﻿using System.Collections;
+using UnityEngine;
 using UnityStandardAssets.Characters.ThirdPerson;
 
 public class Punch : MonoBehaviour
 {
-    float _coolDown = 5;
+    float _coolDown = 2;
     float _nextHitTime = 0;
     public float thrust;
 
     public GameObject hand;
-    public GameObject enemy;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    private RaycastHit _enemy;
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
         Hit();
@@ -27,23 +21,33 @@ public class Punch : MonoBehaviour
     {
         if (Time.time >= _nextHitTime && Input.GetMouseButtonDown(0))
         {
-            RaycastHit hit;
             Vector3 fwd = hand.transform.TransformDirection(Vector3.forward);
             Debug.DrawRay(hand.transform.position, fwd);
-            
-            if (Physics.Raycast(hand.transform.position, fwd, out hit, 2) && hit.collider.CompareTag("Enemy"))
+
+            if (Physics.Raycast(hand.transform.position, fwd, out RaycastHit hit, 2) && hit.collider.CompareTag("Enemy"))
             {
-                Debug.Log("Hit Enemy");
+                _enemy = hit;
                 Vector3 dir = hit.transform.position - transform.position;
+
+                Debug.Log("Hit Enemy");
+
                 hit.collider.gameObject.GetComponent<AICharacterControl>().agent.updatePosition = false;
                 hit.rigidbody.AddForce(dir.normalized * thrust, ForceMode.Impulse);
                 _nextHitTime = Time.time + _coolDown;
-            }
-            else if (Time.time >= _nextHitTime)
-            {
-                hit.collider.gameObject.GetComponent<AICharacterControl>().agent.updatePosition = true;
+
+                StartCoroutine("DisableUpdatePosition");
+
             }
 
         }
     }
+
+    IEnumerator DisableUpdatePosition()
+    {
+        yield return new WaitForSeconds(2f);
+
+        _enemy.collider.gameObject.GetComponent<AICharacterControl>().agent.updatePosition = true;
+    }
+
+
 }
