b55c701b0341fcaddcaa6dd19a0c54a974393549
DiploHatesYou <56423538+DiploHatesYou@users.noreply.github.com>
2019-11-26 12:31:25 -0800

    added player detection

---
+++ Assets/PlayerDetection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class PlayerDetection : MonoBehaviour
+{
+    private AICharacterControl _agent;
+
+    private void Start()
+    {
+        _agent = GetComponent<AICharacterControl>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _agent.target = other.transform;
+        }
+    }
+
+}
