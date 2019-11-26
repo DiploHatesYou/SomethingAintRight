205e62c8ee6d70323c0d1bca4e1ed74f179c2ba6
DiploHatesYou <56423538+DiploHatesYou@users.noreply.github.com>
2019-11-26 12:13:10 -0800

    punch script

--- Assets/Scripts/Punch.cs
+++ Assets/Scripts/Punch.cs
@@ -9,6 +9,8 @@ public class Punch : MonoBehaviour
     float _nextHitTime = 0;
     public float thrust;
 
+    bool enemyHit = false;
+
     public GameObject hand;
     private NavMeshAgent agent;
 
@@ -19,82 +21,99 @@ public class Punch : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //Hit();
-        if (agent.GetComponent<Rigidbody>().velocity.z <= .2f && agent.GetComponent<Rigidbody>().velocity.x <= .2f && agent.GetComponent<Rigidbody>().velocity.y <= .2f)
-        {
-            Debug.Log("Velocity is 0");
-            Vector3 enemyPos = agent.transform.position;
-            agent.Warp(enemyPos);
-            agent.GetComponent<AICharacterControl>().agent.updatePosition = true;
-            agent.GetComponent<AICharacterControl>().agent.updateRotation = true;
+        Hit();
+    }
 
-        }
-        else
+    void Hit()
+    {
+        if (Time.time >= _nextHitTime && Input.GetMouseButtonDown(0))
         {
-            return;
+            Vector3 fwd = hand.transform.TransformDirection(Vector3.forward);
+            Debug.DrawRay(hand.transform.position, fwd);
+
+            if (Physics.Raycast(hand.transform.position, fwd, out RaycastHit hit, 2) && hit.collider.CompareTag("Enemy"))
+            {
+                Vector3 dir = hit.transform.position - transform.position;
+                agent = hit.collider.gameObject.GetComponent<NavMeshAgent>();
+                Debug.Log("Hit Enemy");
+
+                hit.collider.gameObject.GetComponent<AICharacterControl>().agent.updatePosition = false;
+                hit.collider.gameObject.GetComponent<AICharacterControl>().agent.updateRotation = false;
+                hit.rigidbody.AddForce(dir.normalized * thrust, ForceMode.Impulse);
+
+                //_nextHitTime = Time.time + _coolDown;
+
+                if (hit.rigidbody.velocity.z <= .2f && hit.rigidbody.velocity.x <= .2f && hit.rigidbody.velocity.y <= .2f)
+                {
+                    Debug.Log("Velocity is 0");
+                    Vector3 enemyPos = hit.transform.position;
+                    agent.Warp(enemyPos);
+                    agent.GetComponent<AICharacterControl>().agent.updatePosition = true;
+                    agent.GetComponent<AICharacterControl>().agent.updateRotation = true;
+                    //StartCoroutine("DisableUpdatePosition");
+                }
+                else if (Time.time>= _nextHitTime)
+                {
+                    agent.GetComponent<AICharacterControl>().agent.updatePosition = true;
+                    agent.GetComponent<AICharacterControl>().agent.updateRotation = true;
+                }
+            }
         }
     }
 
-    //void Hit()
+    //private void OnCollisionEnter(Collision collision)
     //{
-    //    if (Time.time >= _nextHitTime && Input.GetMouseButtonDown(0))
+    //    if (collision.gameObject.CompareTag("Enemy"))
     //    {
-    //        Vector3 fwd = hand.transform.TransformDirection(Vector3.forward);
-    //        Debug.DrawRay(hand.transform.position, fwd);
-
-    //        if (Physics.Raycast(hand.transform.position, fwd, out RaycastHit hit, 2) && hit.collider.CompareTag("Enemy"))
-    //        {
-    //            Vector3 dir = hit.transform.position - transform.position;
-    //            agent = hit.collider.gameObject.GetComponent<NavMeshAgent>();
-    //            Debug.Log("Hit Enemy");
-
-    //            hit.collider.gameObject.GetComponent<AICharacterControl>().agent.updatePosition = false;
-    //            hit.collider.gameObject.GetComponent<AICharacterControl>().agent.updateRotation = false;
-    //            hit.rigidbody.AddForce(dir.normalized * thrust, ForceMode.Impulse);
-
-    //            //_nextHitTime = Time.time + _coolDown;
-
-    //            if (hit.rigidbody.velocity.z <= .2f && hit.rigidbody.velocity.x <= .2f && hit.rigidbody.velocity.y <= .2f)
-    //            {
-    //                Debug.Log("Velocity is 0");
-    //                Vector3 enemyPos = hit.transform.position;
-    //                agent.Warp(enemyPos);
-    //                agent.GetComponent<AICharacterControl>().agent.updatePosition = true;
-    //                agent.GetComponent<AICharacterControl>().agent.updateRotation = true;
-    //                //StartCoroutine("DisableUpdatePosition");
-    //            }
-
-
-
-
-    //        }
-
+    //        enemyHit = true;
     //    }
-    //}
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Enemy"))
-        {
-            Debug.Log("Hit");
-            agent = other.gameObject.GetComponent<NavMeshAgent>();
-            Vector3 dir = other.transform.position - transform.position;
-            other.gameObject.GetComponent<AICharacterControl>().agent.updatePosition = false;
-            other.gameObject.GetComponent<AICharacterControl>().agent.updateRotation = false;
-            other.attachedRigidbody.AddForce(dir.normalized * thrust, ForceMode.Impulse);
-            
-        }
+        //if (collision.gameObject.CompareTag("Enemy")) /*&& Time.time >= _nextHitTime)*/
+        //{
+        //    Debug.Log("Hit");
+        //    agent = collision.gameObject.GetComponent<NavMeshAgent>();
+        //    Vector3 dir = collision.transform.position - transform.position;
+        //    collision.gameObject.GetComponent<AICharacterControl>().agent.updatePosition = false;
+        //    collision.gameObject.GetComponent<AICharacterControl>().agent.updateRotation = false;
+        //    collision.rigidbody.AddForce(dir.normalized * thrust, ForceMode.Impulse);
+        //    //_nextHitTime = Time.time + _coolDown;
+
+        //    if (agent.GetComponent<Rigidbody>().velocity.z <= .2f && agent.GetComponent<Rigidbody>().velocity.x <= .2f && agent.GetComponent<Rigidbody>().velocity.y <= .2f)
+        //    {
+        //        Debug.Log("Velocity is 0");
+        //        Vector3 enemyPos = agent.transform.position;
+        //        agent.Warp(enemyPos);
+        //        agent.GetComponent<AICharacterControl>().agent.updatePosition = true;
+        //        agent.GetComponent<AICharacterControl>().agent.updateRotation = true;
+        //    }
+        //}
+
+
+        //}
+
+        //private void OnTriggerEnter(Collider other)
+        //{
+        //    if (other.gameObject.CompareTag("Enemy"))
+        //    {
+        //        Debug.Log("Hit");
+        //        agent = other.gameObject.GetComponent<NavMeshAgent>();
+        //        Vector3 dir = other.transform.position - transform.position;
+        //        other.gameObject.GetComponent<AICharacterControl>().agent.updatePosition = false;
+        //        other.gameObject.GetComponent<AICharacterControl>().agent.updateRotation = false;
+        //        other.attachedRigidbody.AddForce(dir.normalized * thrust, ForceMode.Impulse);
+
+        //    }
+        //}
+
+        //IEnumerator DisableUpdatePosition()
+        //{
+
+        //    yield return new WaitForSeconds(.25f);
+
+        //    agent.GetComponent<AICharacterControl>().agent.updatePosition = true;
+        //    agent.GetComponent<AICharacterControl>().agent.updateRotation = true;
+
+        //}
+
+
     }
-
-    IEnumerator DisableUpdatePosition()
-    {
-
-        yield return new WaitForSeconds(.25f);
-
-        agent.GetComponent<AICharacterControl>().agent.updatePosition = true;
-        agent.GetComponent<AICharacterControl>().agent.updateRotation = true;
-
-    }
-
-
-}
