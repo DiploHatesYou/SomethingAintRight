571aab7e811f19c0e735f9e5c08ca2a7f3cb4a71
DiploHatesYou <56423538+DiploHatesYou@users.noreply.github.com>
2019-12-10 11:42:11 -0800

    testing

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health = 1f;
+
+    public int worth = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        PlayerStats.money += worth;
+
+        //GameObject effect = (GameObject)Instantiate(deathEffect, transform.position, Quaternion.identity);
+        //Destroy(effect, 5f);
+
+        Destroy(gameObject);
+    }
+}
