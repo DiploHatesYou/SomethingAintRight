f81504bb55cf9273b13443277913a2528f6ef62a
DiploHatesYou <56423538+DiploHatesYou@users.noreply.github.com>
2019-12-16 22:19:08 -0800

    tweaks

---
+++ Assets/Scripts/LoadNextLevel.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadNextLevel : MonoBehaviour
+{
+    public Canvas txt;
+    private bool inRange = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inRange && Input.GetKeyDown(KeyCode.F))
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            txt.gameObject.SetActive(true);
+            inRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            txt.gameObject.SetActive(false);
+            inRange = false;
+        }
+    }
+}
