94a29d16096d219f7f76e104164d5733e7057a79
DiploHatesYou <56423538+DiploHatesYou@users.noreply.github.com>
2019-12-17 10:00:25 -0800

    kill box on l2

---
+++ Assets/KillBox.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class KillBox : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerStats.health = 0;
+        }
+
+        if (other.CompareTag("Enemy"))
+        {
+            other.GetComponent<Enemy>().health = 0;
+        }
+    }
+}
